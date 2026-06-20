3fef37f494451ab669e5ccfabdd54931efb3cda1
Eugene Lebedev <duke.tougu@gmail.com>
2026-06-20 19:03:46 +0300

    Improve UI/UX

--- frontend/src/lib/components/ui/dropdown-menu/dropdown-menu-checkbox-item.svelte
+++ frontend/src/lib/components/ui/dropdown-menu/dropdown-menu-checkbox-item.svelte
@@ -23,7 +23,7 @@
   bind:indeterminate
   data-slot="dropdown-menu-checkbox-item"
   class={cn(
-    "focus:bg-accent focus:text-accent-foreground focus:**:text-accent-foreground relative flex cursor-default items-center gap-1.5 rounded-md py-1 pr-8 pl-1.5 text-sm outline-hidden select-none data-inset:pl-7 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+    "focus:bg-accent focus:text-accent-foreground focus:**:text-accent-foreground relative flex cursor-default items-center gap-1.5 rounded-md py-2 pr-8 pl-2 text-base outline-hidden select-none data-inset:pl-7 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 sm:py-1 sm:pl-1.5 sm:text-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
     className
   )}
   {...restProps}
--- frontend/src/lib/components/ui/dropdown-menu/dropdown-menu-sub-trigger.svelte
+++ frontend/src/lib/components/ui/dropdown-menu/dropdown-menu-sub-trigger.svelte
@@ -19,7 +19,7 @@
   data-slot="dropdown-menu-sub-trigger"
   data-inset={inset}
   class={cn(
-    "focus:bg-accent focus:text-accent-foreground data-open:bg-accent data-open:text-accent-foreground not-data-[variant=destructive]:focus:**:text-accent-foreground flex cursor-default items-center gap-1.5 rounded-md px-1.5 py-1 text-sm outline-hidden select-none data-inset:pl-7 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+    "focus:bg-accent focus:text-accent-foreground data-open:bg-accent data-open:text-accent-foreground not-data-[variant=destructive]:focus:**:text-accent-foreground flex cursor-default items-center gap-1.5 rounded-md px-2 py-2 text-base outline-hidden select-none data-inset:pl-7 data-[inset]:pl-8 sm:px-1.5 sm:py-1 sm:text-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
     className
   )}
   {...restProps}
